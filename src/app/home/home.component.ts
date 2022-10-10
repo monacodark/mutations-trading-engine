cf53a218fe448cce84eaf0cbc298036f8273d547
monacodark <monacodark@gmail.com>
2022-10-10 12:03:41 +0300

    style fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { createChart } from 'lightweight-charts';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,6 +14,25 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
+
+    const chartContainer = document.getElementById('chart');
+
+    console.log(chartContainer.clientWidth)
+
+    const chart = createChart(chartContainer, { height: 600, width: chartContainer.clientWidth + 30 });
+    const lineSeries = chart.addLineSeries();
+    lineSeries.setData([
+        { time: '2019-04-11', value: 80.01 },
+        { time: '2019-04-12', value: 96.63 },
+        { time: '2019-04-13', value: 76.64 },
+        { time: '2019-04-14', value: 81.89 },
+        { time: '2019-04-15', value: 74.43 },
+        { time: '2019-04-16', value: 80.01 },
+        { time: '2019-04-17', value: 96.63 },
+        { time: '2019-04-18', value: 76.64 },
+        { time: '2019-04-19', value: 81.89 },
+        { time: '2019-04-20', value: 74.43 },
+    ]);
   }
 
   click(): void {
--- src/app/shared/ui/card/components/card-body/card-body.component.ts
+++ src/app/shared/ui/card/components/card-body/card-body.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ms-card-body',
   templateUrl: './card-body.component.html',
   styleUrls: ['./card-body.component.scss'],
 })
-export class CardBodyComponent {}
+export class CardBodyComponent implements OnInit {
+  @Input() noPadding: boolean = false;
+
+  ngOnInit(): void {
+    console.log('yo', this.noPadding);
+  }
+}

bdfa539ca942180e17c89515b7830524c4d480cf
monacodark <monacodark@gmail.com>
2022-10-11 03:52:55 +0300

    smalls fixes

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -16,13 +16,19 @@ export class HomePageComponent implements OnInit {
         { time: '2019-04-11', value: 80.01 },
         { time: '2019-04-12', value: 96.63 },
         { time: '2019-04-13', value: 76.64 },
-        { time: '2019-04-14', value: 81.89 },
+        { time: '2019-04-14', value: 45.89 },
         { time: '2019-04-15', value: 74.43 },
-        { time: '2019-04-16', value: 80.01 },
+        { time: '2019-04-16', value: 34.01 },
         { time: '2019-04-17', value: 96.63 },
         { time: '2019-04-18', value: 76.64 },
         { time: '2019-04-19', value: 81.89 },
-        { time: '2019-04-20', value: 74.43 },
+        { time: '2019-04-20', value: 90.00 },
+        { time: '2019-04-21', value: 74.43 },
+        { time: '2019-04-22', value: 33.01 },
+        { time: '2019-04-23', value: 96.63 },
+        { time: '2019-04-24', value: 76.64 },
+        { time: '2019-04-25', value: 81.89 },
+        { time: '2019-04-26', value: 74.43 },
     ]);
   }
 }

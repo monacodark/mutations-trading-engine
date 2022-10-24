7ccfab3c5579f3cf1ba908873e0c8d2235eb17d1
monacodark <monacodark@gmail.com>
2022-10-25 02:32:02 +0300

    build base struct

---
+++ src/app/shared/ui/text-field/components/text-field.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+
+@Component({
+  selector: 'ms-text-field',
+  templateUrl: './text-field.component.html',
+  styleUrls: ['./text-field.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: TextFieldComponent,
+    },
+  ],
+})
+export class TextFieldComponent implements ControlValueAccessor, OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  value: string = '';
+  touched = false;
+  disabled = false;
+  form!: FormGroup;
+
+  doInput(): void {
+    this.onChange(this.form.value.input)
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      input: [''],
+    });
+  }
+
+  onChange(value: string): void {}
+
+  onTouched = () => {};
+
+  writeValue(value: any) {
+    if (value) {
+      this.form.setValue(value, {emitEvent: false});
+    }
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(disabled: boolean) {
+    if (disabled) {
+      this.form.disable();
+    }
+    else {
+      this.form.enable();
+    }
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+}
